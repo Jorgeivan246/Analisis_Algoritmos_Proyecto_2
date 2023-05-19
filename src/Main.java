6a5a1ddf749a931cdc46552dd231581c8caf1d5e
Zaeboz <jegs2206@gmail.com>
2023-05-19 17:42:14 -0500

    correccion del algoritmo 10

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public class Main {
 
         generarNumero(10000);
 
-        int[] numero = leerArchivo1(10000);
-        int[] numero2 = leerArchivo1(10000);
+        int[] numero = leerArchivo1(10);
+        int[] numero2 = leerArchivo1(100);
 
         ArrayList<Integer> numero3 = leerArchivo2(10);
         ArrayList<Integer> numero4 = leerArchivo2(10);
@@ -18,24 +18,31 @@ public class Main {
         int[] result = null;
 
         Algoritmo8 algo6 = new Algoritmo8();
-
         Algoritmo2 algo2 = new Algoritmo2();
+        Algoritmo10 algo10 = new Algoritmo10();
 
-        ArrayList<Integer> result2 = inicializarArrayList(numero3.size() + numero4.size());
+//        ArrayList<Integer> result2 = inicializarArrayList(numero3.size() + numero4.size());
 
-        result2 = algo6.multiplicar(numero3, numero4, result2);
+//        result2 = algo6.multiplicar(numero3, numero4, result2);
 
-        imprimirArryList(result2);
+//        imprimirArryList(result2);
 
 //       result2=algo2.multiplicar(numero3,numero4);
-//       System.out.println("\n");
-//
-//       for (Integer integer : result2) {
-//
-//           System.out.print(" " + integer);
-//       }
+
+        String resultado = arregloToStrin(algo10.multiplicacionHindu(numero, numero2)) ;
+
+        System.out.println("El resultado de la multiplicacion hindu es: "+resultado);
 
 
+
+    }
+
+    private static String arregloToStrin(int[] multiplicacionHindu) {
+        String result="";
+        for (int i = 0; i < multiplicacionHindu.length; i++) {
+            result=result+multiplicacionHindu[i];
+        }
+        return result;
     }
 
     public static ArrayList<Integer> convertBigIntegerToArrayList(BigInteger bigInt) {
